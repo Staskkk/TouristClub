eecb0276cfdf2adc9b85c5bb962ff4a48db74a51
Станіслав Євгенович Киян <stanislav_kijan@ia.ua>
2018-12-25 13:15:25 +0200

    fix refresh in wpf

---
+++ DB_Cource_1_02/LoginWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core.EntityClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DB_Cource_1_02
+{
+    public partial class sp19TourClubEntities : DbContext
+    {
+        public sp19TourClubEntities(string connectionString) : base(connectionString)
+        {
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static EntityConnectionStringBuilder entityString;
+
+        public static sp19TourClubEntities Connect(string userName, string password)
+        {
+            sp19TourClubEntities dbContext = null;
+            try
+            {
+                if (userName != null)
+                {
+                    entityString = new EntityConnectionStringBuilder()
+                    {
+                        Provider = "System.Data.SqlClient",
+                        Metadata = "res://*/TouristClubModel.csdl|res://*/TouristClubModel.ssdl|res://*/TouristClubModel.msl",
+                        ProviderConnectionString = @"data source=STAS\SQLEXPRESS;initial catalog=sp19TourClub;persist security info=True;MultipleActiveResultSets=True;App=EntityFramework"
+                    };
+                    entityString.ProviderConnectionString += ";user id=" + userName + ";Password=" + password;
+                }
+
+                dbContext = new sp19TourClubEntities(entityString.ConnectionString);
+                if (dbContext.Database.Exists())
+                    return dbContext;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            dbContext?.Dispose();
+            return null;
+        }
+
+        private void Butto_Login_Click(object sender, RoutedEventArgs e)
+        {
+            string login = tbxLogin.Text;
+            var context = Connect(login, tbxPass.Password);
+            if (context == null)
+            {
+                MessageBox.Show("Invalid login or password!");
+                return;
+            }
+
+            new MainWindow(context).Show();
+            Close();
+        }
+
+        private void Button_Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
--- DB_Cource_1_02/MainWindow.xaml.cs
+++ DB_Cource_1_02/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,71 +21,152 @@ namespace DB_Cource_1_02
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private sp19TourClubEntities context;
+
+        CollectionViewSource touristViewSource;
+        CollectionViewSource touristAssigmentViewSource;
+        CollectionViewSource touristCampingTripInfoViewSource;
+        CollectionViewSource touristCompetitionInfoViewSource;
+        CollectionViewSource touristGroupInfoViewSource;
+        CollectionViewSource campingPlaceViewSource;
+        CollectionViewSource campingRouteViewSource;
+        CollectionViewSource campingRoutePlaceInfoViewSource;
+        CollectionViewSource campingTripViewSource;
+        CollectionViewSource competitionViewSource;
+        CollectionViewSource areaTypeViewSource;
+        CollectionViewSource touristLevelViewSource;
+        CollectionViewSource touristPostViewSource;
+        CollectionViewSource scheduleViewSource;
+        CollectionViewSource trainingViewSource;
+        CollectionViewSource tripRecordViewSource;
+        CollectionViewSource groupViewSource;
+        CollectionViewSource sectionViewSource;
+        CollectionViewSource activityTypeViewSource;
+
+        public MainWindow(sp19TourClubEntities context)
         {
+            this.context = context;
             InitializeComponent();
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            this.touristViewSource = ((CollectionViewSource)(this.FindResource("touristViewSource")));
+            this.touristAssigmentViewSource = ((CollectionViewSource)(this.FindResource("touristAssigmentViewSource")));
+            this.touristCampingTripInfoViewSource = ((CollectionViewSource)(this.FindResource("touristCampingTripInfoViewSource")));
+            this.touristCompetitionInfoViewSource = ((CollectionViewSource)(this.FindResource("touristCompetitionInfoViewSource")));
+            this.touristGroupInfoViewSource = ((CollectionViewSource)(this.FindResource("touristGroupInfoViewSource")));
+            this.campingPlaceViewSource = ((CollectionViewSource)(this.FindResource("campingPlaceViewSource")));
+            this.campingRouteViewSource = ((CollectionViewSource)(this.FindResource("campingRouteViewSource")));
+            this.campingRoutePlaceInfoViewSource = ((CollectionViewSource)(this.FindResource("campingRoutePlaceInfoViewSource")));
+            this.campingTripViewSource = ((CollectionViewSource)(this.FindResource("campingTripViewSource")));
+            this.competitionViewSource = ((CollectionViewSource)(this.FindResource("competitionViewSource")));
+            this.areaTypeViewSource = ((CollectionViewSource)(this.FindResource("areaTypeViewSource")));
+            this.touristLevelViewSource = ((CollectionViewSource)(this.FindResource("touristLevelViewSource")));
+            this.touristPostViewSource = ((CollectionViewSource)(this.FindResource("touristPostViewSource")));
+            this.scheduleViewSource = ((CollectionViewSource)(this.FindResource("scheduleViewSource")));
+            this.trainingViewSource = ((CollectionViewSource)(this.FindResource("trainingViewSource")));
+            this.tripRecordViewSource = ((CollectionViewSource)(this.FindResource("tripRecordViewSource")));
+            this.groupViewSource = ((CollectionViewSource)(this.FindResource("groupViewSource")));
+            this.sectionViewSource = ((CollectionViewSource)(this.FindResource("sectionViewSource")));
+            this.activityTypeViewSource = ((CollectionViewSource)(this.FindResource("activityTypeViewSource")));
 
-            System.Windows.Data.CollectionViewSource touristViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("touristViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // touristViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource touristAssigmentViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("touristAssigmentViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // touristAssigmentViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource touristCampingTripInfoViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("touristCampingTripInfoViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // touristCampingTripInfoViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource touristCompetitionInfoViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("touristCompetitionInfoViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // touristCompetitionInfoViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource touristGroupInfoViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("touristGroupInfoViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // touristGroupInfoViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource campingPlaceViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("campingPlaceViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // campingPlaceViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource campingRouteViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("campingRouteViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // campingRouteViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource campingRoutePlaceInfoViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("campingRoutePlaceInfoViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // campingRoutePlaceInfoViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource campingTripViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("campingTripViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // campingTripViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource competitionViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("competitionViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // competitionViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource areaTypeViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("areaTypeViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // areaTypeViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource touristLevelViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("touristLevelViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // touristLevelViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource touristPostViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("touristPostViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // touristPostViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource scheduleViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("scheduleViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // scheduleViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource trainingViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("trainingViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // trainingViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource tripRecordViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("tripRecordViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // tripRecordViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource groupViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("groupViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // groupViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource sectionViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("sectionViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // sectionViewSource.Source = [generic data source]
-            System.Windows.Data.CollectionViewSource activityTypeViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("activityTypeViewSource")));
-            // Load data by setting the CollectionViewSource.Source property:
-            // activityTypeViewSource.Source = [generic data source]
+            LoadAll();
+        }
+
+        private void LoadAll()
+        {
+            TryFunc(context.Tourists.Load);
+            TryFunc(context.TouristAssigments.Load);
+            TryFunc(context.TouristCampingTripInfoes.Load);
+            TryFunc(context.TouristCompetitionInfoes.Load);
+            TryFunc(context.TouristGroupInfoes.Load);
+            TryFunc(context.CampingPlaces.Load);
+            TryFunc(context.CampingRoutes.Load);
+            TryFunc(context.CampingRoutePlaceInfoes.Load);
+            TryFunc(context.CampingTrips.Load);
+            TryFunc(context.Competitions.Load);
+            TryFunc(context.AreaTypes.Load);
+            TryFunc(context.TouristLevels.Load);
+            TryFunc(context.TouristPosts.Load);
+            TryFunc(context.Schedules.Load);
+            TryFunc(context.Trainings.Load);
+            TryFunc(context.TripRecords.Load);
+            TryFunc(context.Groups.Load);
+            TryFunc(context.Sections.Load);
+            TryFunc(context.ActivityTypes.Load);
+
+            touristViewSource.Source = context.Tourists.Local;
+            touristAssigmentViewSource.Source = context.TouristAssigments.Local;
+            touristCampingTripInfoViewSource.Source = context.TouristCampingTripInfoes.Local;
+            touristCompetitionInfoViewSource.Source = context.TouristCompetitionInfoes.Local;
+            touristGroupInfoViewSource.Source = context.TouristGroupInfoes.Local;
+            campingPlaceViewSource.Source = context.CampingPlaces.Local;
+            campingRouteViewSource.Source = context.CampingRoutes.Local;
+            campingRoutePlaceInfoViewSource.Source = context.CampingRoutePlaceInfoes.Local;
+            campingTripViewSource.Source = context.CampingTrips.Local;
+            competitionViewSource.Source = context.Competitions.Local;
+            areaTypeViewSource.Source = context.AreaTypes.Local;
+            touristLevelViewSource.Source = context.TouristLevels.Local;
+            touristPostViewSource.Source = context.TouristPosts.Local;
+            scheduleViewSource.Source = context.Schedules.Local;
+            trainingViewSource.Source = context.Trainings.Local;
+            tripRecordViewSource.Source = context.TripRecords.Local;
+            groupViewSource.Source = context.Groups.Local;
+            sectionViewSource.Source = context.Sections.Local;
+            activityTypeViewSource.Source = context.ActivityTypes.Local;
+        }
+
+        public static bool TryFunc(Action method)
+        {
+            try
+            {
+                method();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+            return false;
+        }
+
+        private void BtnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.context.Dispose();
+                this.context = LoginWindow.Connect(null, null);
+                LoadAll();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(GetRealException(ex).Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(GetRealException(ex).Message, "Exception", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private static Exception GetRealException(Exception ex)
+        {
+            Exception res = ex;
+            while (res.InnerException != null)
+            {
+                res = res.InnerException;
+            }
+
+            return res;
         }
     }
 }
